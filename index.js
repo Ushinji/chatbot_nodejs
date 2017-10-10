768685c617260e08b0b1b736915ef60abf357152
Ushinji <siguma971@gmail.com>
2017-10-11 07:34:01 +0900

    debug

--- index.js
+++ index.js
@@ -2,151 +2,97 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
+var crypto = require("crypto");
 var async = require('async');
 
-app.set('port', (process.env.PORT || 5000));
-app.use(bodyParser.urlencoded({extended: true}));  // JSONの送信を許可
-app.use(bodyParser.json());                        // JSONのパースを楽に（受信時）
-// app.get('/', function(request, response) {
-//     response.send('Hello World!');
-// });
+app.set('port', (process.env.PORT || 8000));
+// JSONの送信を許可
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// JSONのパースを楽に（受信時）
+app.use(bodyParser.json());
 
-app.post('/callback', function(req, res){
-
+app.post('/callback', function(req, res) {
     async.waterfall([
-        // ぐるなびAPI
-        function(callback) {
-            var json = req.body;
-            console.log('app run 1.');
-/*
-            // 受信テキスト
-            var search_place = json['result'][0]['content']['text'];
-            var search_place_array = search_place.split("\n");
+            function(callback) {
+                // リクエストがLINE Platformから送られてきたか確認する
+                if (!validate_signature(req.headers['x-line-signature'], req.body)) {
+                    return;
+                }
+                // テキストが送られてきた場合のみ返事をする
+                if ((req.body['events'][0]['type'] != 'message') || (req.body['events'][0]['message']['type'] != 'text')) {
+                    return;
+                }
+                // 「くっころ」という単語がテキストに含まれている場合のみ返事をする
+                if (req.body['events'][0]['message']['text'].indexOf('くっころ') == -1) {
+                    return;
+                }
 
-            //検索キーワード
-            var gnavi_keyword = "";
-            if(search_place_array.length == 2){
-                var keyword_array = search_place_array[1].split("、");
-                gnavi_keyword = keyword_array.join();
-            }
-*/
-            // ぐるなびAPI レストラン検索API
-            var gnavi_url = 'https://api.gnavi.co.jp/RestSearchAPI/20150630/';
-            // ぐるなび リクエストパラメータの設定
-            var gnavi_query = {
-                "keyid": process.env.GNAVI_ACCESS_KEY,
-                "format": "json",
-                "address": "東京都渋谷区",
-                "hit_per_page": 1,
-                "freeword": "ラーメン",
-                "freeword_condition": 2
+                // 1対1のチャットの場合は相手のユーザ名で返事をする
+                // グループチャットの場合はユーザ名が分からないので、「貴様ら」で返事をする
+                if (req.body['events'][0]['source']['type'] == 'user') {
+                    // ユーザIDでLINEのプロファイルを検索して、ユーザ名を取得する
+                    var user_id = req.body['events'][0]['source']['userId'];
+                    var get_profile_options = {
+                        url: 'https://api.line.me/v2/bot/profile/' + user_id,
+                        proxy: process.env.FIXIE_URL,
+                        json: true,
+                        headers: {
+                            'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}'
+                        }
+                    };
+                    request.get(get_profile_options, function(error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            callback(body['displayName']);
+                        }
+                    });
+                } else if ('room' == req.body['events'][0]['source']['type']) {
+                    callback('貴様ら');
+                }
+            },
+        ],
+        function(displayName) {
+            //ヘッダーを定義
+            var headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}',
             };
 
-            var gnavi_options = {
-                url: gnavi_url,
-                headers : {'Content-Type' : 'application/json; charset=UTF-8'},
-                qs: gnavi_query,
-                json: true
+            // 送信データ作成
+            var data = {
+                'replyToken': req.body['events'][0]['replyToken'],
+                "messages": [{
+                    "type": "text",
+                    "text": displayName + 'にこんな辱めを受けるとは...！\nくっ...殺せ！'
+                }]
             };
 
-            // 検索結果をオブジェクト化
-            var search_result = {};
+            //オプションを定義
+            var options = {
+                url: 'https://api.line.me/v2/bot/message/reply',
+                proxy: process.env.FIXIE_URL,
+                headers: headers,
+                json: true,
+                body: data
+            };
 
-            request.get(gnavi_options, function (error, response, body){
+            request.post(options, function(error, response, body) {
                 if (!error && response.statusCode == 200) {
-                }
-                else{
+                    console.log(body);
+                } else {
+                    console.log('error: ' + JSON.stringify(response));
                 }
             });
-
-            console.log('app run 2.');
-
-            search_result['name'] = "検索結果.店名";
-            search_result['address'] = "検索結果.場所";
-            callback(null, json, search_result);
-            /*
-            request.get(gnavi_options, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    if('error' in body){
-                        console.log("検索エラー" + JSON.stringify(body));
-                        return;
-                    }
-                    // 店名
-                    if('name' in body.rest){
-                        search_result['name'] = body.rest.name;
-                    }
-                    // 住所
-                    if('address' in body.rest){
-                        search_result['address'] = body.rest.address;
-                    }
-                    callback(null, json, search_result);
-                }
-                else {
-                    console.log('error: '+ response.statusCode);
-                }
-            });
-            */
-        },
-    ],
-
-    // LINE BOT
-    function(err, json, search_result) {
-        if(err){
-            return;
         }
-
-        //ヘッダーを定義
-        var headers = {
-            'Content-Type' : 'application/json; charset=UTF-8',
-            'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}',
-        };
-        console.log('app run 3.');
-
-        // 送信相手の設定（配列）
-        var to_array = [];
-        to_array.push(json['result'][0]['content']['from']);
-
-        console.log('app run 4.');
-
-        // 送信データ作成
-        var data = {
-            'to': to_array,
-            'toChannel': 1383378250, //固定
-            'eventType':'140177271400161403', //固定
-            "content": {
-                "messageNotified": 0,
-                "messages": [
-                    // テキスト
-                    {
-                        "contentType": 1,
-                        "text": 'こちらはいかがですか？\n【お店】' + search_result['name'] + '\n【場所】' + search_result['address'],
-                    }
-                ]
-            }
-        };
-        console.log('app run 5.');
-
-        //オプションを定義
-        var options = {
-            url: 'https://trialbot-api.line.me/v1/events',
-            proxy : process.env.FIXIE_URL,
-            headers: headers,
-            json: true,
-            body: data
-        };
-
-        request.post(options, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body);
-            } else {
-                console.log('error: '+ JSON.stringify(response));
-            }
-        });
-        console.log('app run 5.');
-    });
-
+    );
 });
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running');
 });
+
+// 署名検証
+function validate_signature(signature, body) {
+    return signature == crypto.createHmac('sha256', process.env.LINE_CHANNEL_SECRET).update(new Buffer(JSON.stringify(body), 'utf8')).digest('base64');
+}
