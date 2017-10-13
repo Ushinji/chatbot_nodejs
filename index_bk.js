bb76c4c804ef44e21bdc5088e9ef1b737c2855fa
Ushinji <siguma971@gmail.com>
2017-10-14 07:40:53 +0900

    Add luis

--- index_bk.js
+++ index_bk.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
+var crypto = require("crypto");
 var async = require('async');
 
 app.set('port', (process.env.PORT || 8000));
@@ -20,40 +21,62 @@ app.post('/callback', function(req, res) {
                     || (req.body['events'][0]['message']['type'] != 'text')) {
                     return;
                 }
-                // 検索キーワード
-                var in_word = req.body['events'][0]['message']['text'];
 
-                var url = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/37005d38-9ccf-4963-af7a-d6b1bf049654";
+                // 検索キーワード
+                var input_array = req.body['events'][0]['message']['text'].split("\n");
+                var address = input_array[0];
+                var freeword = input_array[1];
 
                 // ぐるなび リクエストパラメータの設定
-                var luis_query = {
-                    "subscription-key":process.env.LUIS_SUBSCRIPTION_KEY,
-                    "staging":true,
-                    "timezoneOffset":0,
-                    "verbose":true,
-                    "q":in_word
+                var gnavi_query = {
+                    "keyid":process.env.GNAVI_ACCESS_KEY,
+                    "format":"json",
+                    "address":address,
+                    "hit_per_page":1,
+                    "freeword":freeword,
+                    "freeword_condition":2
                 };
 
-                //オプションを定義
-                var luis_options = {
-                    url: url,
+                // ぐるなびAPI レストラン検索API
+                var gnavi_url = 'https://api.gnavi.co.jp/RestSearchAPI/20150630/';
+
+                var gnavi_options = {
+                    url: gnavi_url,
                     headers : {'Content-Type' : 'application/json; charset=UTF-8'},
-                    qs: luis_query,
-                    json: true,
+                    qs: gnavi_query,
+                    json: true
                 };
 
                 // 検索結果をオブジェクト化
-                var luis_result = {};
+                var search_result = {};
 
-                request.get(luis_options, function (error, response, body) {
+                request.get(gnavi_options, function (error, response, body) {
                     if (!error && response.statusCode == 200) {
                         if('error' in body){
                             console.log("検索エラー" + JSON.stringify(body));
                             return;
                         }
-                        console.log('StatudCod: '+ response.statusCode);
-                        console.log(response);
-                        callback(null, luis_result);
+                        // 店名
+                        if('name' in body.rest){
+                            search_result['name'] = body.rest.name;
+                        }
+                        // 住所
+                        if('address' in body.rest){
+                            search_result['address'] = body.rest.address;
+                        }
+                        // 緯度
+                        if('latitude' in body.rest){
+                            search_result['latitude'] = body.rest.latitude;
+                        }
+                        // 軽度
+                        if('longitude' in body.rest){
+                            search_result['longitude'] = body.rest.longitude;
+                        }
+                        // 営業時間
+                        if('opentime' in body.rest){
+                            search_result['opentime'] = body.rest.opentime;
+                        }
+                        callback(null, search_result);
                     } else {
                         console.log('error: '+ response.statusCode);
                     }
@@ -61,7 +84,6 @@ app.post('/callback', function(req, res) {
             },
         ],
         function(err, search_result) {
-/*
             //ヘッダーを定義
             var headers = {
                 'Content-Type': 'application/json',
@@ -74,9 +96,18 @@ app.post('/callback', function(req, res) {
                     {
                         "type":"text",
                         "text": 'こちらはいかがですか？\n【お店】' + search_result['name'] + '\n【営業時間】' + search_result['opentime'],
+                    },
+                    // 位置情報
+                    {
+                        "type":"location",
+                        "title":search_result['name'],
+                        "address":search_result['address'],
+                        "latitude": Number(search_result['latitude']),
+                        "longitude": Number(search_result['longitude'])
                     }
                 ]
             };
+
             //オプションを定義
             var options = {
                 url: 'https://api.line.me/v2/bot/message/reply',
@@ -85,7 +116,7 @@ app.post('/callback', function(req, res) {
                 json: true,
                 body: data
             };
-            // LINEメッセージ送信元へメッセージを送信
+
             request.post(options, function(error, response, body) {
                 if (!error && response.statusCode == 200) {
                     console.log(body);
@@ -93,7 +124,6 @@ app.post('/callback', function(req, res) {
                     console.log('error: ' + JSON.stringify(response));
                 }
             });
-*/
         }
     );
 });
