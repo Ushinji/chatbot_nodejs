bedd964c26bb0f0f9cc4d444fc809684e6dac396
Ushinji <siguma971@gmail.com>
2017-10-15 09:57:21 +0900

    Add Wiki-Search

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var async = require('async');
 var wiki = require("node-wikipedia");
-var str_space = " ";
 
 app.set('port', (process.env.PORT || 8000));
 // JSONの送信を許可
@@ -22,8 +21,9 @@ app.post('/callback', function(req, res) {
                 || (req.body['events'][0]['message']['type'] != 'text')) {
                 return;
             }
-            var phrase = req.body['events'][0]['message']['text'];
 
+            // 入力された本文を取得し、LUISのAPIヘ送信
+            var phrase = req.body['events'][0]['message']['text'];
             var luis_url = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/37005d38-9ccf-4963-af7a-d6b1bf049654";
             var luis_query = {
                 "subscription-key":process.env.LUIS_SUBSCRIPTION_KEY,
@@ -38,7 +38,6 @@ app.post('/callback', function(req, res) {
                 qs: luis_query,
                 json: true,
             };
-
             var result = {};
             request.get(luis_options, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
@@ -51,7 +50,10 @@ app.post('/callback', function(req, res) {
                     }
                     body.entities.forEach(function(entity){
                         if( entity.type == "search_word" ){
-                            result['search_word'] = entity.entity.replace(str_space,"");
+                            // 検索ワードの中にスペースがある場合は、取り除く
+                            var str_space = " ";
+                            result['search_word']
+                                = entity.entity.replace(str_space,"");
                         }
                     });
                     callback(null, result);
@@ -61,6 +63,7 @@ app.post('/callback', function(req, res) {
                 }
             });
         },
+        // Wikipediaで検索
         function(result, callback) {
             var wiki_url = 'http://wikipedia.simpleapi.net/api';
             var wiki_query = {
@@ -77,9 +80,9 @@ app.post('/callback', function(req, res) {
                 if (!error && response.statusCode == 200) {
                     console.log(response.body);
                     if(response.body != null ){
-                        result['wiki_content'] = response.body[0].body.substr(0,140);
-                    }else{
-                        result['wiki_content'] =　"";
+                        // Wiki本文のうち、140文字を抽出
+                        result['wiki_content']
+                            = response.body[0].body.substr(0,140);
                     }
                     callback(null, result);
                 }
@@ -89,13 +92,15 @@ app.post('/callback', function(req, res) {
                 }
             });
         }],
+        // LINEの応答の作成
         function(err, result) {
             var headers = {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}',
             };
+
             var data = {};
-            if( result['wiki_content'] != "" ){
+            if('search_word' in result && 'wiki_content' in result){
                 data = {
                     'replyToken': req.body['events'][0]['replyToken'],
                     "messages": [
@@ -109,18 +114,18 @@ app.post('/callback', function(req, res) {
                         }
                     ]
                 };
-            }
-            else{
+            }else{
                 data = {
                     'replyToken': req.body['events'][0]['replyToken'],
                     "messages": [
                         {
                             "type":"text",
-                            "text":result['search_word'] + 'は難しい...',
+                            "text":'ぐぬぬ...',
                         }
                     ]
                 };
             }
+
             var options = {
                 url: 'https://api.line.me/v2/bot/message/reply',
                 proxy: process.env.FIXIE_URL,
